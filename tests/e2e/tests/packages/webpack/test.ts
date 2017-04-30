4164a3ae6b9f14de9def7ea8bf91b6f6cb79dabb
FrozenPandaz <jasonjean1993@gmail.com>
2017-04-29 21:34:19 -0400

    upgrade to ng4

--- tests/e2e/tests/packages/webpack/test.ts
+++ tests/e2e/tests/packages/webpack/test.ts
@@ -7,7 +7,7 @@ import {expectFileSizeToBeUnder, replaceInFile, expectFileToMatch} from '../../.
 export default function(skipCleaning: () => void) {
   return Promise.resolve()
     .then(() => createProjectFromAsset('webpack/test-app'))
-    .then(() => exec(normalize('node_modules/.bin/webpack'), '-p'))
+    .then(() => exec(normalize('npm run webpack'), '-- -p'))
     .then(() => expectFileSizeToBeUnder('dist/app.main.js', 420000))
     .then(() => expectFileSizeToBeUnder('dist/0.app.main.js', 10000))
     // test resource urls without ./
